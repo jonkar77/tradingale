82a0a5951ebcf8c9844fade737845c14d972154a
OJ <onkarjoshi296@gmail.com>
2024-04-16 10:07:58 +0530

    modified calendar

--- src/app/components/Calendar/page.tsx
+++ src/app/components/Calendar/page.tsx
@@ -11,7 +11,7 @@ const Calendar: React.FC<CalendarProps> = ({ onSelectDate }) => {
     'September', 'October', 'November', 'December'
   ];
 
-  const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
   const handleClickDate = (date: Date) => {
     onSelectDate(date);
@@ -37,18 +37,18 @@ const Calendar: React.FC<CalendarProps> = ({ onSelectDate }) => {
       {months.map((month, index) => {
         const numDays = new Date(2022, index + 1, 0).getDate(); // Get the number of days in the current month
         const firstDayOfMonth = new Date(2022, index, 1).getDay(); // Get the day of the week for the first day of the month
-        const emptyCells = getEmptyCells(weekDays[firstDayOfMonth]);
+        const emptyCells = getEmptyCells(weekDays[firstDayOfMonth-1]);
         return (
           <div key={month} className="border border-gray-300 rounded-lg p-3 shadow-xl shadow-emerald-900">
             <h2 className="text-md font-semibold mb-2">{month}</h2>
-            <div className="grid grid-cols-7">
+            <div className="grid grid-cols-7 ">
               {weekDays.map(day => (
-                <div key={day} className="text-xs text-center font-semibold text-gray-500 py-2">
-                  {day}
+                <div key={day} className="text-xs text-center font-semibold text-gray-700 py-2">
+                  <div className='border border-slate-400 rounded-lg'>{day}</div>
                 </div>
               ))}
               {emptyCells}
-              {Array.from({ length: numDays }, (_, day) => day + 1).map(day => {
+              {Array.from({ length: numDays }, (_, day) => day +1).map(day => {
                 const dayOfWeek = getDayOfWeek(2022, index, day);
                 return (
                   <div
