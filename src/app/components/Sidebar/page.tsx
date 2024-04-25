33a5095108ede53fdf15039b15046ff9e04feb36
OJ <onkarjoshi296@gmail.com>
2024-04-25 18:02:37 +0530

    Add new icons and update menu items in Sidebar component

--- src/app/components/Sidebar/page.tsx
+++ src/app/components/Sidebar/page.tsx
@@ -1,8 +1,12 @@
 'use client'
-// Import necessary modules and components
-import { ChevronFirst, ChevronLast } from "lucide-react";
 import { createContext, useContext, useState } from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { RxDashboard } from "react-icons/rx";
+import { MdAccountBox } from "react-icons/md";
+import { AiOutlineMenuUnfold } from "react-icons/ai";
+import { RiSkipLeftLine } from "react-icons/ri";
+import { MdDynamicFeed } from "react-icons/md";
 
 // Declare the type of the context value
 interface SidebarContextType {
@@ -17,12 +21,19 @@ const SidebarContext = createContext<SidebarContextType | undefined>(undefined);
 export default function Sidebar() {
   const [expanded, setExpanded] = useState(true);
   const [activePath, setActivePath] = useState(""); // State to track active path
+  // Get the current path name
 
   // Data object with icons and paths
   const menuItems = [
-    { text: "Dashboard", icon: <ChevronFirst />, path: "/profile/dashboard" },
-    { text: "Feed", icon: <ChevronLast />, path: "/profile/feed" },
-    { text: "Account", icon: <ChevronLast />, path: "/profile/account" },
+    {
+      text: "Dashboard", icon: <RxDashboard size={22} />
+      , path: "/profile/dashboard"
+    },
+    { text: "Feed", icon: <MdDynamicFeed size={22} />, path: "/profile/feed" },
+    {
+      text: "Account", icon: <MdAccountBox size={22} />
+      , path: "/profile/account"
+    },
     // Add more items as needed
   ];
 
@@ -31,17 +42,21 @@ export default function Sidebar() {
       <aside className="h-screen">
         <nav className="h-full flex flex-col bg-teal-600 border-r shadow-sm">
           <div className="p-4 pb-2 flex justify-between items-center">
-            <button onClick={() => setExpanded((curr) => !curr)} className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100">
-              {expanded ? <ChevronFirst /> : <ChevronLast />}
+            <button onClick={() => setExpanded((curr) => !curr)} className="pl-3 rounded-lg">
+              {expanded ? <RiSkipLeftLine size={18} />
+                : <AiOutlineMenuUnfold />
+              }
             </button>
           </div>
+          <div className="mt-5">
 
-          <ul className="flex-1 px-3">
-            {/* Render SidebarItems based on the menuItems data */}
-            {menuItems.map((item, index) => (
-              <SidebarItem key={index} text={item.text} icon={item.icon} path={item.path} />
-            ))}
-          </ul>
+            <ul className="flex-1 px-3">
+              {/* Render SidebarItems based on the menuItems data */}
+              {menuItems.map((item, index) => (
+                <SidebarItem key={index} text={item.text} icon={item.icon} path={item.path} />
+              ))}
+            </ul>
+          </div>
         </nav>
       </aside>
     </SidebarContext.Provider>
@@ -51,19 +66,22 @@ export default function Sidebar() {
 // Define the SidebarItem component
 export function SidebarItem({ icon, text, path }: { icon: JSX.Element; text: string; path: string }) {
   const { expanded, activePath, setActivePath } = useContext(SidebarContext) as SidebarContextType;
-  const isActive = activePath === path;
+  const pathname = usePathname();
+  const isActive = pathname === path;
 
   const handleClick = () => {
     setActivePath(path); // Update active path on click
   };
 
   return (
-    <li className={`relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group hover:bg-indigo-50 text-gray-600 ${isActive ? "bg-indigo-100 text-indigo-800" : ""}`} onClick={handleClick}>
-      <Link href={path} passHref
-        className="flex items-center">
+    <div>
+      <li className={`relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group hover:bg-indigo-50 text-gray-600 ${isActive ? "bg-indigo-100 text-indigo-800" : ""}`} onClick={handleClick}>
+        <Link href={path} passHref
+          className="flex items-center">
           {icon}
           <span className={`overflow-hidden transition-all ${expanded ? "w-[150px] ml-1" : "w-0"}`}>{text}</span>
-      </Link>
-    </li>
+        </Link>
+      </li>
+    </div>
   );
 }
